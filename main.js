c46b3e88c5f07e7ce47db1b10a05ec0c6c122a00
Chaeyeon Seo <120750451+ChaeYami@users.noreply.github.com>
2022-12-30 18:05:58 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -4,44 +4,70 @@ const url = require('url');
 
 
 const app = http.createServer(function(request,response){
-const _url = request.url;
-const queryData = new URL('http://localhost:3000' + _url).searchParams;
-    // console.log(queryData.get('id'));
-let title = queryData.get('id'); //if문에서 바꿔서 사용하기 때문에 let 
-
-if(_url == '/'){
-    title = "Welcome";
-}
-if(_url == '/favicon.ico'){
-    response.writeHead(404);
-    response.end();
-return;
-}
-response.writeHead(200);
-fs.readFile(`data/${queryData.get('id')}`,'utf-8',function(err,description){
-  const template = `<!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    <ul>
-      <li><a href="/?id=HTML">HTML</a></li>
-      <li><a href="/?id=CSS">CSS</a></li>
-      <li><a href="/?id=JavaScript">JavaScript</a></li>
-    </ul>
-    <h2>${title}</h2>
-    <p>${description}</p>
-  </body>
-  </html>
-  `
-  response.end(template);
-})
+  const _url = request.url;
+  const queryData = new URL('http://localhost:3000' + _url).searchParams;
+  const pathname = new URL('http://localhost:3000' + _url).pathname;
 
 
+  if(pathname === '/'){    //경로가 루트라면
+    if(queryData.get('id')===null){ //id값이 없다면 
+      
+      fs.readFile(`data/${queryData.get('id')}`,'utf-8',function(err,description){
+        let title = 'Welcome';
+        var description = 'Hello, Node.js';
+            //let 쓰면 Identifier 'description' has already been declared 에러 뜸
+        const template = `<!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul style="list-style: none;">
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+       })
 
+    } else{
+      fs.readFile(`data/${queryData.get('id')}`,'utf-8',function(err,description){
+        let title = queryData.get('id');
+        const template = `<!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul style="list-style: none;">
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+    })
+    }
+    
+  } else{
+    response.writeHead(404); 
+    response.end("Not found");
+  }
 
 });
 app.listen(3000);
