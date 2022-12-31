b330f51475dfbf22675e6c474899bf26e9f60c69
Chaeyeon Seo <ssallyseo@gmail.com>
2022-12-31 23:12:58 +0900

    .

--- main.js
+++ main.js
@@ -9,65 +9,68 @@ const app = http.createServer(function(request,response){
   const pathname = new URL('http://localhost:3000' + _url).pathname;
 
 
-  if(pathname === '/'){    //경로가 루트라면
-    if(queryData.get('id')===null){ //id값이 없다면 
-      
-      fs.readFile(`data/${queryData.get('id')}`,'utf-8',function(err,description){
-        let title = 'Welcome';
-        var description = 'Hello, Node.js';
-            //let 쓰면 Identifier 'description' has already been declared 에러 뜸
-        const template = `<!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul style="list-style: none;">
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
-        response.writeHead(200);
-        response.end(template);
-       })
-
-    } else{
-      fs.readFile(`data/${queryData.get('id')}`,'utf-8',function(err,description){
-        let title = queryData.get('id');
-        const template = `<!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul style="list-style: none;">
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
-        response.writeHead(200);
-        response.end(template);
-    })
+  fs.readdir('./data',(err,fileList)=>{
+    let list = '<ul style="list-style: none;">';
+    for(let i=0; i<fileList.length ; i++){
+      list = list + `<li><a href = "/?id=${fileList[i]}" >${fileList[i]}</a></li>`;
     }
-    
-  } else{
-    response.writeHead(404); 
-    response.end("Not found");
-  }
+    list = list+'</ul>'
+    // const list=`<ul style="list-style: none;">
+        //   <li><a href="/?id=HTML">HTML</a></li>
+        //   <li><a href="/?id=CSS">CSS</a></li>
+        //   <li><a href="/?id=JavaScript">JavaScript</a></li>
+        //   </ul>`
 
+    if(pathname === '/'){    //경로가 루트라면
+      if(queryData.get('id')===null){ //id값이 없다면 
+        
+          let title = 'Welcome';
+          var description = 'Hello, Node.js';
+                //let 쓰면 Identifier 'description' has already been declared 에러 뜸
+          
+          const template = `<!doctype html>
+          <html>
+          <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+          </head>
+          <body>
+          <h1><a href="/">WEB</a></h1>
+          ${list}
+          <h2>${title}</h2>
+          <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        
+      } else {
+          fs.readFile(`data/${queryData.get('id')}`,'utf-8',function(err,description){
+            let title = queryData.get('id');
+            const template = `<!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              ${list}
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+            </html>
+            `;
+            response.writeHead(200);
+            response.end(template);
+          })
+      }
+      
+    } else{
+      response.writeHead(404); 
+      response.end("Not found");
+    }
+  })
 });
 app.listen(3000);
