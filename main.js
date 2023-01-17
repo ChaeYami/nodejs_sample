9b2ba2ad49bd1d4aafe318ee2b8e2b628eec5eca
Chaeyeon Seo <120750451+ChaeYami@users.noreply.github.com>
2023-01-17 15:22:43 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,21 +2,6 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
-function templateHTMl(_title, _list, _body){
-  return `<!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${_title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    ${_list}
-    ${_body}
-  </body>
-  </html>
-  `
-}
 
 const app = http.createServer(function(request,response){
   const _url = request.url;
@@ -24,46 +9,65 @@ const app = http.createServer(function(request,response){
   const pathname = new URL('http://localhost:3000' + _url).pathname;
 
 
-  fs.readdir('./data',(err,fileList)=>{
-    let list = '<ul style="list-style: none;">';
-    for(let i=0; i<fileList.length ; i++){
-      list = list + `<li><a href = "/?id=${fileList[i]}" >${fileList[i]}</a></li>`;
-    }
-    list = list+'</ul>'
-    // const list=`<ul style="list-style: none;">
-        //   <li><a href="/?id=HTML">HTML</a></li>
-        //   <li><a href="/?id=CSS">CSS</a></li>
-        //   <li><a href="/?id=JavaScript">JavaScript</a></li>
-        //   </ul>`
-
-    if(pathname === '/'){    //경로가 루트라면
-
-      if(queryData.get('id')===null){ //id값이 없다면 
-        
-          let title = 'Welcome';
-          var description = 'Hello, Node.js';
-                //let 쓰면 Identifier 'description' has already been declared 에러 뜸
-          const body = `<h2>${title}</h2><p>${description}</p>`;
-          const template = templateHTMl(title, list, body);
-
-          response.writeHead(200);
-          response.end(template);
-        
-      } else {
-          fs.readFile(`data/${queryData.get('id')}`,'utf-8',function(err,description){
-            let title = queryData.get('id');
-            const body = `<h2>${title}</h2><p>${description}</p>`;
-            const template = templateHTMl(title, list, body);
-            
-            response.writeHead(200);
-            response.end(template);
-          })
-      }
+  if(pathname === '/'){    //경로가 루트라면
+    if(queryData.get('id')===null){ //id값이 없다면 
+      
+      fs.readFile(`data/${queryData.get('id')}`,'utf-8',function(err,description){
+        let title = 'Welcome';
+        var description = 'Hello, Node.js';
+            //let 쓰면 Identifier 'description' has already been declared 에러 뜸
+        const template = `<!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul style="list-style: none;">
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+       })
 
     } else{
-      response.writeHead(404); 
-      response.end("Not found");
+      fs.readFile(`data/${queryData.get('id')}`,'utf-8',function(err,description){
+        let title = queryData.get('id');
+        const template = `<!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul style="list-style: none;">
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+    })
     }
-  })
+    
+  } else{
+    response.writeHead(404); 
+    response.end("Not found");
+  }
+
 });
 app.listen(3000);
